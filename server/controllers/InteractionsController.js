24423e9fee01e3d10830368695af0785c3588785
Kevin Kelley <ksquaredcoding@gmail.com>
2022-09-23 14:26:58 -0600

    bro comments

--- server/controllers/InteractionsController.js
+++ server/controllers/InteractionsController.js
@@ -11,9 +11,57 @@ export class InteractionsController extends BaseController {
       .use(Auth0Provider.getAuthorizedUserInfo)
       .post('/haters', this.beHatin)
       .post('/bros', this.beABro)
-      .post('/comments')
+      .post('/comments', this.addComment)
+      .post('/comments/commenthaters', this.hateComment)
+      .post('/comments/commentbros', this.broComment)
       .get('/haters', this.getHaters)
       .get('/bros', this.getBros)
+      .delete('/comments/:commentId', this.deleteComment)
+      .delete('/comments')
+  }
+  async broComment(req, res, next) {
+    try {
+      const formData = {
+        commentBroId: req.userInfo.id,
+        commentId: req.body.commentId
+      }
+      const commentBro = await interactionsService.broComment(formData)
+      res.send(commentBro)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async hateComment(req, res, next) {
+    try {
+      const formData = {
+        commentHaterId: req.userInfo.id,
+        commentId: req.body.commentId
+      }
+      const commentHater = await interactionsService.hateComment(formData)
+      res.send(commentHater)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async deleteComment(req, res, next) {
+    try {
+      const deletedComment = await interactionsService.deleteComment(req.params.commentId, req.userInfo)
+      res.send(deletedComment)
+    } catch (error) {
+      next(error)
+    }
+  }
+  async addComment(req, res, next) {
+    try {
+      const formData = {
+        commenterId: req.userInfo.id,
+        memeId: req.body.memeId
+      }
+      const comment = await interactionsService.addComment(formData)
+      res.send(comment)
+    } catch (error) {
+      next(error)
+    }
   }
   async getComments(req, res, next) {
     try {
