2c255b7e30a1024f18e8b280cc4fadff5e97a7c5
Kevin Kelley <ksquaredcoding@gmail.com>
2022-09-23 14:27:44 -0600

    bro comments

--- server/services/InteractionsService.js
+++ server/services/InteractionsService.js
@@ -2,9 +2,36 @@ import { dbContext } from '../db/DbContext'
 import { BadRequest, Forbidden } from '../utils/Errors'
 
 class InteractionsService {
+  async broComment(formData) {
+    const bro = await dbContext.CommentBros.create(formData)
+    await bro.populate('comment')
+    await bro.populate('commentBro', 'name')
+    return bro
+  }
+  async hateComment(formData) {
+    const hater = await dbContext.CommentHaters.create(formData)
+    await hater.populate('comment')
+    await hater.populate('commentHater', 'name')
+    return hater
+  }
+  async deleteComment(commentId, userInfo) {
+    const comment = await dbContext.Comments.findById(commentId)
+    if (!comment) { throw new BadRequest("You can't delete comments that don't exist...") }
+    if (userInfo.id != comment.commenterId.toString()) {
+      throw new Forbidden("That ain't your comment!")
+    }
+    await comment.remove()
+    return comment
+  }
+  async addComment(formData) {
+    const comment = await dbContext.Comments.create(formData)
+    await comment.populate('meme')
+    return comment
+  }
   async getComments(query = {}) {
     const comments = await dbContext.Comments.find(query)
       .populate('meme').populate('commentHaters').populate('commentBros')
+    return comments
   }
   async beABro(formData) {
     const bro = await dbContext.Bros.create(formData)
